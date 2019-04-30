d1d151982d3f412df72698a6170738e01618acba
SamuelTroyer677 <john@nowhere.net>
2019-04-30 17:09:26 -0400

    did final project

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,46 @@
 <template>
   <v-app>
+    <v-toolbar>
+      <v-img fluid max-height="50" max-width="50" v-bind:src="'https://pbs.twimg.com/profile_images/463038072846688256/NDuIYRqn.jpeg'"></v-img>       
+      <v-toolbar-title class="display-1 font-weight-light">MVNU Cafe Menu</v-toolbar-title>
+        </v-toolbar>
+    <v-container>
+      <!-- <cafeTitle v-bind:pic="'https://pbs.twimg.com/profile_images/463038072846688256/NDuIYRqn.jpeg'" /> -->
+      <tweetMenu
+        v-for="tweet in timeline"
+        v-bind:key="tweet.id"
+        v-bind:text="tweet.full_text"
+        v-bind:tweetDate="tweet.created_at.substring(0, tweet.created_at.indexOf('+'))"
+      ></tweetMenu>
+    </v-container>
   </v-app>
 </template>
 
 <script>
-import BlankComponent from './components/BlankComponent'
-
+import tweetMenu from "./components/tweetMenu";
+import cafeTitle from "./components/cafeTitle";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    BlankComponent
+    tweetMenu,
+    cafeTitle
   },
-  data () {
+
+  data() {
     return {
-      //
-    }
+      timeline: []
+    };
+  },
+  mounted: async function() {
+    let response = await fetch(
+      "http://98.27.202.77/mvnu/testingFinal/test.php",
+      { mode: "cors" }
+    );
+    this.timeline = await response.json();
+    console.log(this.timeline);
   }
-}
+};
 </script>
+
+<style>
+</style>
---
+++ src/components/tweetMenu.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-container>
+    <v-layout align-center justify-center column>
+    <v-flex xs2 sm6 md4>
+    <v-card class="text-sm-left elevation-12">
+      <table class="menuTable">
+        <tr>
+          <th class="blue lighten-3 text-xs-center">
+            {{tweetDate}}
+          </th>
+        </tr>
+        <tr v-for="item in menuArray" :key="item">
+          <td>{{item}}</td>
+        </tr>
+      </table> 
+    </v-card>
+    </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "tweetMenu",
+ 
+  props: {
+    text: String,
+    tweetDate: String
+  },
+ data () {
+    return {
+      singleTweet: this.text,
+      menuArray: []
+    };
+  },
+  mounted: function() {
+    //this.singleTweet = this.text;
+    //console.log(this.singleTweet);
+    this.menuArray = this.text.split("\n");
+    for (let i = 0; i < this.menuArray.length; i++) {
+      this.menuArray[i] = this.menuArray[i].replace(/&amp/g, '&').replace('&;', '&');
+    }
+    //let cutStr = s.indexOf('+');
+    //console.log(this.menuArray);
+    
+  }
+};
+</script>
+
+<style>
+.tweetLines {
+  white-space: pre-wrap;
+}
+.menuTable {
+  border: 1px solid;
+    border-spacing: 0;
+    max-width: 380px;
+}
+th,td {
+    border: 1px solid;
+    font-family: arial;
+    font-size: 17px;
+}
+.dateTitle {
+  border: 3px solid;
+}
+</style>
